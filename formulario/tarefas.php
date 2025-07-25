c741ddcbaa914db97ffa5480eaa9ad09c86b9c22
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-07-25 18:54:28 -0300

    Update tarefas.php

--- formulario/tarefas.php
+++ formulario/tarefas.php
@@ -22,8 +22,14 @@
     <?php
     $lista_tarefas = array();
         if(isset($_GET['nome'])){
-            $lista_tarefas[] = $_GET['nome'];
-        }        
+            $_SESSION['lista_tarefas'][] = $_GET['nome'];
+        }    
+        $lista_tarefas = array();
+        if(isset($_SESSION['lista_tarefas'])) {
+            $lista_tarefas = $_SESSION[
+                'lista_tarefas'
+            ];
+        }   
     ?>  
     <table>
         <tr>
