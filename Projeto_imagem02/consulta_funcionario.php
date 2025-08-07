4c91af5ffd80c2e2a38884a17db86855ccedd759
Gabriel Bartholdy <gabrielbartholdy07@gmail.com>
2025-08-07 20:45:49 -0300

    a

--- Projeto_imagem02/consulta_funcionario.php
+++ Projeto_imagem02/consulta_funcionario.php
@@ -14,10 +14,24 @@ try {
     $sql = "SELECT id,nome FROM funcionarios";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
-    $funcionarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+    $funcionarios = $stmt->fetchAll(PDO::FETCH_ASSOC);//busca todos os resultados como uma matriz associativa
 
+    //verifica se foi solicitado a exclusão de um funcionario
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['excluir_id'])) {
+        $excluir_id = $_POST['excluir_id'];
+        //prepara a query de exclusão
+        $sql_excluir = "DELETE FROM funcionarios WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id', $excluir_id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            echo "Funcionario excluido com sucesso!";
+            header("Location: consulta_funcionario.php");
+            exit;
+        } else {
+            echo "Erro ao excluir funcionario.";
+        }
 
+    }
 
 }
 
